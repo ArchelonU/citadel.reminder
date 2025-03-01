6d991e7cc22ccc7c11a7f07b1fc3a396ac112d58
ArchelonU <archelon@petryaev.ru>
2025-03-01 16:46:29 +0300

    Split start/end messages

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ def main():
 
 def load_timetables():
     global timetables
-    with open(os.path.join(os.path.dirname(__file__), 'timetables.json')) as f:
-        timetables = json.load(f)
-    f.close()
+    with open(os.path.join(os.path.dirname(__file__), 'timetables.json')) as file:
+        timetables = json.load(file)
+    file.close()
 
 def sequence():
     global current_date, current_weekday, current_time, duty_section
@@ -45,23 +45,34 @@ def monday_notifications():
         match current_time:
             case "09:00" :
                 message = "На этой неделе дежурит секция:\n" + str(duty_section['icon']) + " " + str(duty_section['name'])
-                vk_bot_session.method("messages.send", {"peer_id":int(timetables['main_vk_chat_id']), "message":message,"random_id":0})
+                send_vk_message(int(timetables['main_vk_chat_id']),message)
             case "14:00" :
                 previus_date = current_date - timedelta(days=7)
                 if current_date.month != previus_date.month : # Первый понедельник месяца
                     for section in timetables['sections']:
                         message = "Напоминание о сдаче взносов. 💰\n\n👹 Казна сама себя не наполнит!"
-                        vk_bot_session.method("messages.send", {"peer_id":section['chat_id'], "message":message,"random_id":0})
+                        send_vk_message(section['chat_id'],message)
 
 def duty_notification():
     for workout in duty_section['workouts']:
-        workout_weekday = workout['weekday']
-        if current_weekday == int(workout_weekday):
-            workout_start = workout['start']
-            workout_end = workout['end']
-            if current_time == str(workout_start) or current_time == str(workout_end):
+        workout_weekday = int(workout['weekday'])
+        if current_weekday == workout_weekday:
+            workout_start = str(workout['start'])
+            workout_end = str(workout['end'])
+            if current_time == workout_start:
                 message = "Дружественное напоминание о дежурстве 🙌"
-                vk_bot_session.method("messages.send", {"peer_id":duty_section['chat_id'], "message":message,"random_id":0})
+                send_vk_message(duty_section['chat_id'],message)
+            if current_time == workout_end:
+                message = "Перед уходом, пожалуйста, убедитесь, что:\n" \
+                            "- Снаряжение убрано по местам\n" \
+                            "- Посуда помыта и убрана в шкаф\n" \
+                            "- Мусорное ведро остаётся пустым\n" \
+                            "\n" \
+                            "Благодарим за вклад в общее дело! 🤝"
+                send_vk_message(duty_section['chat_id'],message)
+
+def send_vk_message(chat_id, message):
+    vk_bot_session.method("messages.send", {"peer_id":chat_id, "message":message,"random_id":0})
 
 if __name__ == '__main__':
     main()
